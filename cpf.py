f5ebdd13a4e37a6464c37de2ae08b122d4ea1587
VoDKaGrayHat <43305632+VoDKaGrayHat@users.noreply.github.com>
2018-09-16 10:21:32 -0300

    Add files via upload

---
+++ cpf.py
@@ -0,0 +1,72 @@
+#Script feito pelo VoDKa
+#Proibido modificacoes no codigo
+#Todos os direitos reservados ao VoDKa
+#Nao me responsabilizo pelo mau uso da ferramenta  
+print"""
+_________________________________________________________
+Site:http://vodkagrayhat.epizy.com/                    /|
+______________________________________________________/ |
+ ###  ###   ######   ######   ###  ###     ######     | |
+ ###  ###  ###  ###  ### ###  ### ###     ###  ###    | |
+ ###  ###  ###  ###  ###  ### ######     ###    ###   | |
+ ###  ###  ###  ###  ###  ### ######    ############  | |
+  ######   ###  ###  ### ###  ### ###   ####    ####  | |
+   ####     ######   ######   ###  ###  ####    ####  | |
+GitHub:https://github.com/VoDKaGrayHat/               | |
+                                                      | /
+____________F_T_P_______G_r_a_y_H_a_t_________________|/
+"""
+print"By:VoDKa"
+cpf=raw_input("\n\nCPF: ")
+n=0
+for num in cpf:
+	n=n+1
+	if n==9:
+		print"""
+			\ \   / /__ |  _ \| |/ /__ _ 
+			 \ \ / / _ \| | | | ' // _` |
+			  \ V / (_) | |_| | . \ (_| |
+  			   \_/ \___/|____/|_|\_\__,_|
+
+		"""
+		db=num
+		if db == "0":
+			print"O CPF foi cadastrado em:"
+			print"Rio Grande do Sul"
+		if db == "1":
+			print"O CPF foi cadastrado em:"
+			print"Distrito Federal, Goias, Mato Grosso, Mato Grosso do Sul ou Tocantins"
+
+		if db == "2":
+			print"O CPF foi cadastrado em:"
+			print"Amazonas, Para, Roraima, Amapa, Acre ou Rondonia"
+
+		if db == "3":
+			print"O CPF foi cadastrado em:"
+			print"Ceara, Maranhao ou Piaui"
+
+		if db == "4":
+			print"O CPF foi cadastrado em:"
+			print"Paraiba, Pernambuco, Alagoas ou Rio Grande do Norte"
+		
+		if db == "5":
+			print"O CPF foi cadastrado em:"
+			print"Bahia ou Sergipe"
+
+		if db == "6":
+			print"O CPF foi cadastrado em:"
+			print"Minas Gerais"
+
+		if db == "7":
+			print"O CPF foi cadastrado em:"
+			print"Rio de Janeiro ou Espirito Santo"
+
+		if db == "8":
+			print"O CPF foi cadastrado em:"
+			print"Sao Paulo"
+
+		if db == "9":
+			print"O CPF foi cadastrado em:"
+			print"Parana ou Santa Catarina "
+
+			
